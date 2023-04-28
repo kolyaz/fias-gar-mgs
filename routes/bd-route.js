69c3823cfd4053ba04109b5f82b8885aafd95dff
kolyaz <energo_ceh@mail.ru>
2023-04-28 16:24:05 +0300

    первая стабильная версия2

--- routes/bd-route.js
+++ routes/bd-route.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routerBD = Router();
 
-import getSyncBDcontroller from '../controllers/getSyncBD-controller.js';
-import getDropBDcontroller from '../controllers/getDropBD-controller.js';
+import getSyncBDcontroller from '../controllers/GetSyncBD-controller.js';
+import getDropBDcontroller from '../controllers/GetDropBD-controller.js';
 
 routerBD.post('/getSyncBD', getSyncBDcontroller);
 routerBD.post('/getDropBD', getDropBDcontroller);
--- routes/fias-gar-route.js
+++ routes/fias-gar-route.js
@@ -2,12 +2,12 @@ import { Router } from 'express';
 
 const routerGar = Router();
 
-import getDateGarController from '../controllers/getGarDate-controller.js';
-import getDownloadFullBaseGarController from '../controllers/getDownloadFullBaseGar-controller.js';
-import getDownloadUpdateGarController from '../controllers/getDownloadUpdateGar-controller.js';
-import { getVerDateController } from '../controllers/getVerDate-controller.js';
-import parseGarBaseController from '../controllers/parseGarBase-controller.js';
-import parseGarUpdateController from '../controllers/parseGarUpdate-controller.js';
+import getDateGarController from '../controllers/GetGarDate-controller.js';
+import getDownloadFullBaseGarController from '../controllers/GetDownloadFullBaseGar-controller.js';
+import getDownloadUpdateGarController from '../controllers/GetDownloadUpdateGar-controller.js';
+import { getVerDateController } from '../controllers/GetVerDate-controller.js';
+import parseGarBaseController from '../controllers/ParseGarBase-controller.js';
+import parseGarUpdateController from '../controllers/ParseGarUpdate-controller.js';
 
 routerGar.post('/getFullBaseGar', getDownloadFullBaseGarController);
 routerGar.post('/getUpdateGar', getDownloadUpdateGarController);
